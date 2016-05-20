1bb04c73f89a60f9e03d375865a25acb82d0c7df
Boothand <boothand@hotmail.com>
2016-05-21 01:36:59 +0200

    Gamemanager

---
+++ PacjamUnity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public Scene nextScene;
+	public static int candyAmount;
+	
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		if (candyAmount <= 0)
+		{
+			//Inititate happy animations, ballons, stuff...
+			print("Took all candiez");
+		}
+	}
+}
