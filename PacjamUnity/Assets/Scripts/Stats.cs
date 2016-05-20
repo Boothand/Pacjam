a02b9d65368c4f9d8e6fa65a9cba8196cc45288a
Boothand <boothand@hotmail.com>
2016-05-20 18:52:40 +0200

    Stats. So, score.

---
+++ PacjamUnity/Assets/Scripts/Stats.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+	[SerializeField] int score;
+
+	public int Score
+	{
+		get { return score; }
+		set
+		{
+			value = Mathf.Clamp(value, 0, 10000);
+			score = value;
+		}
+	}
+	
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
