318973e5c9c523d10f2155e8fd3af90be808bcf1
Boothand <boothand@hotmail.com>
2016-05-21 01:35:12 +0200

    Slabs can spawn candy

---
+++ PacjamUnity/Assets/Scripts/Slab.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Slab : MonoBehaviour
+{
+	public GameObject candy;
+	public float groundOffset = 0.5f;
+	
+	void Start ()
+	{
+		if (candy)
+		{
+			Instantiate(candy, transform.position + Vector3.up * groundOffset, candy.transform.rotation);
+			GameManager.candyAmount++;
+		}
+	}
+}
