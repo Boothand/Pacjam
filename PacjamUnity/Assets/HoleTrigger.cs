0d8cc8d9745208bcbc7834e9f4f84d53dba47391
axaka <martinvadseth@gmail.com>
2016-05-20 22:51:49 +0200

    script that detects enemy-collision

---
+++ PacjamUnity/Assets/HoleTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class HoleTrigger : MonoBehaviour
+{
+	public GameObject collisionObject;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Enemy")
+		{
+			collisionObject = col.gameObject;
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if (col.tag == "Enemy")
+		{
+			collisionObject = null;
+		}
+	}
+
+}
