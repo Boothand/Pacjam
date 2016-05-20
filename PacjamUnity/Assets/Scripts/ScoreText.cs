607be1d658985c876b814d1ee0fa233f2df803d2
Boothand <boothand@hotmail.com>
2016-05-20 18:52:29 +0200

    A candy you can swoop up and get points from

---
+++ PacjamUnity/Assets/Scripts/ScoreText.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreText : MonoBehaviour
+{
+	bool fading;
+	CanvasGroup canvasGroup;
+	Text txt;
+
+	public float speed = 1f;
+	public float fadeSpeed = 1f;
+	
+	void Awake ()
+	{
+		canvasGroup = GetComponent<CanvasGroup>();
+		txt = transform.GetComponentInChildren<Text>();
+	}
+
+	public void Fade()
+	{
+		if (!fading)
+		{
+			fading = true;
+			StartCoroutine(FadeAndMove());
+		}
+	}
+
+	public void SetText(string text)
+	{
+		txt.text = text;
+	}
+
+	public void SetColor(Color color)
+	{
+		txt.color = color;
+	}
+
+	IEnumerator FadeAndMove()
+	{
+		while (canvasGroup.alpha > 0.01f)
+		{
+			canvasGroup.alpha -= Time.deltaTime * fadeSpeed;
+			transform.position += Vector3.up * Time.deltaTime * speed;
+			yield return new WaitForEndOfFrame();
+		}
+
+		gameObject.SetActive(false);
+	}
+	
+	void Update ()
+	{
+	}
+}
