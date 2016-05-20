a3db2a37a26a9c320d59499e68f4fdb713bf5344
axaka <martinvadseth@gmail.com>
2016-05-21 01:32:15 +0200

    Made a new slab, used for spawning the player called StarSlab

---
+++ PacjamUnity/Assets/Scripts/StarSlab.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class StarSlab : MonoBehaviour
+{
+	public bool started = false;
+	public float rotationSpeed = 360;
+	float rotation = 0;
+	public float rotationEnd = 1080;
+
+	public GameObject player;
+	GameObject ppl;
+	public bool fastSpawn;
+	bool spawnEnd = false;
+	public AnimationCurve startAnimation;
+	public float animationSpeed = 0.2f;
+	float animationStep = 0f;
+
+	void Start ()
+	{
+		if (fastSpawn) //used while debugging
+		{
+			Instantiate(player, transform.position + transform.up * 0.5f, player.transform.rotation);
+			started = true;
+			rotation = rotationEnd;
+			Destroy(this);
+		}
+		else
+		{
+			ppl = Instantiate(player, transform.position - transform.up * 10f, player.transform.rotation) as GameObject;
+		}
+	}
+	
+	void Update ()
+	{
+		if (!spawnEnd && ppl)
+		{
+			animationStep += Time.deltaTime;
+			ppl.transform.position = new Vector3(transform.position.x, startAnimation.Evaluate(animationStep * animationSpeed), transform.position.z);
+			if (animationStep * animationSpeed > 1)
+				Destroy(this);
+		}
+
+		if (started)
+		{
+			if (rotation <= rotationEnd)
+			{
+				rotation += Time.deltaTime * rotationSpeed;
+				rotationSpeed -= rotationSpeed * 0.5f * Time.deltaTime;
+				transform.rotation = Quaternion.AngleAxis(rotation, Vector3.right);
+			}
+			else
+			{
+				transform.rotation = Quaternion.identity;
+			}
+
+			
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (!started)
+		{
+			started = true;
+		}
+	}
+}
