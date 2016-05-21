fe75418713d0aacbe3b78bc674f20f216d095472
Boothand <boothand@hotmail.com>
2016-05-21 21:13:34 +0200

    UI + UI anims

--- PacjamUnity/Assets/Scripts/UI/ScoreIndicator.cs
+++ PacjamUnity/Assets/Scripts/UI/ScoreIndicator.cs
@@ -5,14 +5,21 @@ public class ScoreIndicator : MonoBehaviour
 {
 	public Text textField;
 	public Image image;
+	Animator anim;
 	
 	void Start ()
 	{
-		
+		anim = GetComponent<Animator>();
+		GameManager.instance.scoreScript = this;
+	}
+
+	public void PlayWobbleAnimation()
+	{
+		anim.SetTrigger("Wobble");
 	}
 	
 	void Update ()
 	{
-		
+		textField.text = GameManager.score.ToString() + "x";
 	}
 }
