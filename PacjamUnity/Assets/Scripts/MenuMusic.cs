4337deefe8a0dd520838d877276096ddb49ac561
Boothand <boothand@hotmail.com>
2016-05-22 07:08:48 +0200

    Main menu setup

---
+++ PacjamUnity/Assets/Scripts/MenuMusic.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class MenuMusic : MonoBehaviour
+{
+	float timer;
+	float musicTimer;
+
+	public AudioSource drumsOnly;
+	public AudioSource music;
+
+	public float speed = 0.1f;
+	public float secondsUntilFade = 5f;
+	
+	void Start ()
+	{
+		music.volume = 0;
+	}
+	
+	void Update ()
+	{
+
+		if (timer > secondsUntilFade)
+		{
+			musicTimer += Time.deltaTime * speed;
+
+			drumsOnly.volume -= musicTimer;
+			music.volume += musicTimer;
+		}
+		else
+		{
+			timer += Time.deltaTime;
+		}
+
+		if (music.volume > 1)
+		{
+			music.volume = 1;
+			drumsOnly.volume = 0;
+		}
+
+	}
+}
